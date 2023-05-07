ad0166b7baf43362fcb589174420c2e9104f8151
aarif <arifkarivellil@gmail.com>
2023-05-07 23:54:24 +0530

    first update

---
+++ Main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+ami           = "ami-007855ac798b5175e" 
+instance_type = "t2.micro"
+}
+tags = {
+    Name = "TF-Instance1"
+    resource "aws_s3_bucket" "S3 Bucket" {
+  bucket = "aarifkarbuc7"
+
+  tags = {
+    Name        = "terrabuc"
+  }
+    }
+}
+
+
